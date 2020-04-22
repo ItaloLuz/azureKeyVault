a48ea3b5c020884e21ff85f97304c4cdda760e57
Italo Bruno Duarte da Luz <italo.luz@softplan.com.br>
2020-04-22 15:18:26 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace KeyVaultApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var builtConfig = config.Build();
+                    var vaultUrl = $"https://{builtConfig["VaultName"]}.vault.azure.net/";
+                    config.AddAzureKeyVault(vaultUrl);
+                })
+                .UseStartup<Startup>();
+    }
+}
